98741549aca22af4adb5e982202649ca696aa955
Jack Scarpelli <Festivetomcat99@gmail.com>
2023-01-21 17:39:21 -0500

    commit tree 522dc030c448a993f500028840f71ef4ddfc2d52

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  1. Iterate through the array with a nested loop
+  2. Compare the sum of current element and next element with target
+  3. If sum equals to target, return true
+  4. If no match found, return false
 */
 
 /*
   Add written explanation of your solution here
+  The function takes in an array and a target value as arguments. It uses nested loop to iterate through the array and check if any two elements' sum equals to the target value. If a match is found, it returns true. Otherwise, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
